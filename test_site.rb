9bd13a6a3e0fa2409b120e4681f9d771905b2a66
indivar <indimishra@gmail.com>
2026-07-20 00:25:41 +0530

    Fix poster sizing on mobile

--- test_site.rb
+++ test_site.rb
@@ -10,6 +10,7 @@ raise "missing BangPypers link" unless html.include?("https://bangalore.pythonin
 raise "wrong X link" unless html.include?("https://x.com/__bangpypers__")
 raise "wrong contact email" unless html.scan("mailto:banglorepy@gmail.com").size == 2
 raise "wrong favicon" unless html.include?("/Assets/logos/bangpypers.svg")
+raise "missing mobile poster inset" unless html.include?("width: calc(100% - 1rem)")
 raise "carousel interval changed" unless html.include?("5000")
 ([event.fetch("hero_image")] + partners.map { |partner| partner.fetch("image") }).each do |image|
   raise "missing image: #{image}" unless File.file?(image.delete_prefix("/"))
